e63167e05755f13be13ee4baf348e00b0afc71d0
chang05go <3653568344@qq.com>
2025-04-22 20:19:28 +0800

    Update mod_robot_arm.cpp

--- src/module/robot_arm/mod_robot_arm.cpp
+++ src/module/robot_arm/mod_robot_arm.cpp
@@ -3,26 +3,29 @@
 #include <thread.hpp>
 
 #include "bsp_time.h"
-#include "dev_rm_motor.hpp"
-
 using namespace Module;
 
 RobotArm::RobotArm(Param& param, float control_freq)
     : param_(param),
-      mode_(RobotArm::WORK_BOT),
-      roll2_actr_(this->param_.roll2_actr, control_freq),
-
+      mode_(RELAX),
+      yaw1_actr_(this->param_.yaw1_actr, control_freq),      //
+      yaw2_actr_(this->param_.yaw2_actr, control_freq),      //
+      pitch1_actr_(this->param_.pitch1_actr, control_freq),  //
+      pitch2_actr_(this->param_.pitch2_actr, control_freq),  //
+      roll1_actr_(this->param_.roll1_actr, control_freq),    //
+      roll2_actr_(this->param_.roll2_actr, control_freq),  // 必须按照顺序初始化
       yaw1_motor_(this->param_.yaw1_motor, "RobotArm_Yaw1"),
+      yaw2_motor_(this->param_.yaw2_motor, "RobotArm_Yaw2"),
       pitch1_motor_(this->param_.pitch1_motor, "RobotArm_Pitch1"),
       pitch2_motor_(this->param_.pitch2_motor, "RobotArm_Pitch2"),
       roll1_motor_(this->param_.roll1_motor, "RobotArm_Roll1"),
-      yaw2_motor_(this->param_.yaw2_motor, "RobotArm_Yaw2"),
       roll2_motor_(this->param_.roll2_motor, "RobotArm_Roll2"),
       custom_ctrl_(param.cust_ctrl),
       ctrl_lock_(true) {
   memset(&(this->cmd_), 0, sizeof(this->cmd_));
 
-  memset(&(this->setpoint_), 0, sizeof(this->setpoint_));
+  // memset(&(this->setpoint_), 0,
+  // sizeof(this->setpoint_));//这行代码让setpoint的所有值为0了；
 
   auto event_callback = [](RobotArmEvent event, RobotArm* robotarm) {
     robotarm->ctrl_lock_.Wait(UINT32_MAX);
@@ -40,8 +43,14 @@ RobotArm::RobotArm(Param& param, float control_freq)
       case SET_MODE_WORK_BOT:
         robotarm->SetMode(WORK_BOT);
         break;
+      case SET_MODE_XIKUANG:
+        robotarm->SetMode(XIKUANG);
+        break;
       case SET_MODE_SAFE:
         robotarm->SetMode(SAFE);
+        break;
+      case SET_MODE_CUSTOM_CTRL:
+        robotarm->SetMode(WORK_CUSTOM_CTRL);
         break;
       default:
         break;
@@ -55,15 +64,15 @@ RobotArm::RobotArm(Param& param, float control_freq)
 
   auto robotarm_thread = [](RobotArm* robotarm) {
     auto cmd_sub = Message::Subscriber<Component::CMD::GimbalCMD>("cmd_gimbal");
-
+    // robotarm->DamiaoSetAble();不行
     uint32_t last_online_time = bsp_time_get_ms();
-
     while (1) {
       cmd_sub.DumpData(robotarm->cmd_);
+
       robotarm->ctrl_lock_.Wait(UINT32_MAX);
       robotarm->DamiaoSetAble();
-      robotarm->UpdateFeedback();
       robotarm->Control();
+
       robotarm->ctrl_lock_.Post();
 
       robotarm->thread_.SleepUntil(2, last_online_time);
@@ -74,187 +83,205 @@ RobotArm::RobotArm(Param& param, float control_freq)
                        System::Thread::MEDIUM);
 }
 
-void RobotArm::DamiaoSetAble() {
-  if (this->yaw1_able_ == 0) {
-    this->yaw1_motor_.Disable();
-  } else {
-    this->yaw1_motor_.Enable();
-  };
-  if (this->pitch1_able_ == 0) {
-    this->pitch1_motor_.Disable();
-  } else {
-    this->pitch1_motor_.Enable();
-  };
-  if (this->pitch2_able_ == 0) {
-    this->pitch2_motor_.Disable();
-  } else {
-    this->pitch2_motor_.Enable();
-  };
-  if (this->roll1_able_ == 0) {
-    this->roll1_motor_.Disable();
-  } else {
-    this->roll1_motor_.Enable();
-  };
-  if (this->yaw2_able_ == 0) {
-    this->yaw2_motor_.Disable();
-  } else {
-    this->yaw2_motor_.Enable();
-  };
-}
+void RobotArm::
+    DamiaoSetAble() {  // 这个函数不是使能函数，而是根据0,1设置电机使能状态//配合relax函数，兼职完美。
 
-void RobotArm::UpdateFeedback() {
-  this->yaw1_motor_.Update();
-  this->pitch1_motor_.Update();
-  this->pitch2_motor_.Update();
-  this->roll1_motor_.Update();
-  this->yaw2_motor_.Update();
-  this->roll2_motor_.Update();
+  if (this->state_.motor_last == 0 && this->state_.motor_current == 1) {
+    this->pitch2_motor_.Enable();
+    this->pitch1_motor_.Enable();
+    this->roll1_motor_.Enable();
+    this->yaw1_motor_.Enable();
+    this->yaw2_motor_.Enable();
+    this->state_.motor_last = 1;
+  }
+  if (this->state_.motor_last == 1 && this->state_.motor_current == 0) {
+    this->pitch2_motor_.Relax();
+    this->pitch1_motor_.Relax();
+    this->roll1_motor_.Relax();
+    this->yaw1_motor_.Relax();
+    this->yaw2_motor_.Relax();
+    this->state_.motor_last = 0;
+  }
+  // if (this->pitch2_able_ == 0) {
+  //   if (this->pitch2_able_last == 1) {
+  //     this->pitch2_motor_.Relax();
+  //     this->pitch2_able_last = 0;
+  //   }
+  // }
+  // if (this->pitch2_able_ == 1) {
+  //   if (this->pitch2_able_last == 0) {
+  //     this->pitch2_motor_.Enable();
+  //     this->pitch2_able_last = 1;
+  //   }
+  // };
+
+  // // pitch1
+  // if (this->pitch1_able_ == 0) {
+  //   if (this->pitch1_able_last == 1) {
+  //     this->pitch1_motor_.Relax();
+  //     this->pitch1_able_last = 0;
+  //   }
+  // }
+  // if (this->pitch1_able_ == 1) {
+  //   if (this->pitch1_able_last == 0) {
+
+  //     this->pitch1_able_last = 1;
+  //   }
+  // };
+  // // roll1
+  // if (this->roll1_able_ == 0) {
+  //   if (this->roll1_able_last == 1) {
+  //     this->roll1_motor_.Relax();
+  //     this->roll1_able_last = 0;
+  //   }
+  // }
+  // if (this->roll1_able_ == 1) {
+  //   if (this->roll1_able_last == 0) {
+
+  //     this->roll1_able_last = 1;
+  //   }
+  // };
+
+  // // yaw1
+  // if (this->yaw1_able_ == 0) {
+  //   if (this->yaw1_able_last == 1) {
+  //     this->yaw1_motor_.Relax();
+  //     this->yaw1_able_last = 0;
+  //   }
+  // }
+  // if (this->yaw1_able_ == 1) {
+  //   if (this->yaw1_able_last == 0) {
+
+  //     this->yaw1_able_last = 1;
+  //   }
+  // };
+  // // yaw2
+  // if (this->yaw2_able_ == 0) {
+  //   if (this->yaw2_able_last == 1) {
+  //     this->yaw2_motor_.Relax();
+  //     this->yaw2_able_last = 0;
+  //   }
+  // }
+  // if (this->yaw2_able_ == 1) {
+  //   if (this->yaw2_able_last == 0) {
+  //     this->yaw2_motor_.Enable();
+  //     this->yaw2_able_last = 1;
+  //   }
+  // };
+}
+void RobotArm::DMable() {
+  this->pitch1_motor_.Relax();
+  this->pitch2_motor_.Relax();
+  this->yaw2_motor_.Relax();
+  this->yaw1_motor_.Relax();
+  this->roll1_motor_.Relax();
 }
 
 void RobotArm::Control() {
   this->now_ = bsp_time_get();
-
+  // 时间差
   this->dt_ = TIME_DIFF(this->last_wakeup_, this->now_);
 
   this->last_wakeup_ = this->now_;
 
-  // this->setpoint_.yaw1_theta_ = this->cmd_.theta.yaw1;
-  // this->setpoint_.pitch1_theta_ = this->cmd_.theta.pitch1;
-  // this->setpoint_.pitch2_theta_ = this->cmd_.theta.pitch2;
-  // this->setpoint_.pitch3_theta_ = this->cmd_.theta.pitch3;
-  // this->setpoint_.yaw2_theta_ = this->cmd_.theta.yaw2;
-  // this->setpoint_.roll_theta_ = this->cmd_.theta.roll;
-
   float pit_cmd = 0.0f;
   float yaw_cmd = 0.0f;
-
   yaw_cmd = this->cmd_.eulr.yaw * this->dt_;
   pit_cmd = this->cmd_.eulr.pit * this->dt_;
 
-  /*电机限幅*/
-  // clampf(&(this->setpoint_.yaw1_theta_), this->param_.limit.yaw1_min,
-  //        this->param_.limit.yaw1_max);
-  // clampf(&(this->setpoint_.pitch1_theta_), this->param_.limit.pitch1_min,
-  //        this->param_.limit.pitch1_max);
-  // clampf(&(this->setpoint_.pitch2_theta_), this->param_.limit.pitch2_min,
-  //        this->param_.limit.pitch2_max);
-  // clampf(&(this->setpoint_.roll1_theta_), this->param_.limit.roll1_min,
-  //        this->param_.limit.roll1_max);
-  // clampf(&(this->setpoint_.yaw2_theta_), this->param_.limit.yaw2_min,
-  //        this->param_.limit.yaw2_max);
-
-  // this->yaw1_motor_.SetPosSpeed(this->setpoint_.yaw1_theta_, 0.1f);
-  // this->pitch1_motor_.SetPosSpeed(this->setpoint_.pitch1_theta_, 0.3f);
-  // this->pitch2_motor_.SetPosSpeed(this->setpoint_.pitch2_theta_, 0.3f);
-  // this->roll1_motor_.SetPosSpeed(this->setpoint_.roll1_theta_, 0.3f);
-  // this->yaw2_motor_.SetPosSpeed(this->setpoint_.yaw2_theta_, 0.3f);
-
-  // float roll2_out = this->roll2_actr_.Calculate(
-  //     this->setpoint_roll2_, this->roll2_motor_.GetSpeed(),
-  //     this->roll2_motor_.GetAngle(), this->dt_);
-  // this->roll2_motor_.Control(roll2_out);
-
   switch (this->mode_) {
-    case RobotArm::WORK_BOT: {
-      this->setpoint_.yaw1_theta_ += yaw_cmd;
-      this->setpoint_.pitch1_theta_ += pit_cmd;
+    case RobotArm::WORK_TOP: {
+      this->setpoint_.yaw1_theta_ = Component::Type::CycleValue(
+          this->setpoint_.yaw1_theta_ + yaw_cmd * 2.0f);
+      this->setpoint_.yaw1_out_ = this->yaw1_actr_.Calculate(
+          this->setpoint_.yaw1_theta_, this->yaw1_motor_.raw_speed,
+          this->yaw1_motor_.raw_pos_, this->dt_);
+      this->yaw1_motor_.SetMit(this->setpoint_.yaw1_out_);
 
-      clampf(&(this->setpoint_.yaw1_theta_), this->param_.limit.yaw1_min,
-             this->param_.limit.yaw1_max);
-      clampf(&(this->setpoint_.pitch1_theta_), this->param_.limit.pitch1_min,
-             this->param_.limit.pitch1_max);
-
-      this->yaw1_motor_.SetPosSpeed(this->setpoint_.yaw1_theta_, 0.1f);
-      this->pitch1_motor_.SetPosSpeed(this->setpoint_.pitch1_theta_, 0.3f);
+      this->setpoint_.pitch1_theta_ = Component::Type::CycleValue(
+          this->setpoint_.pitch1_theta_ + pit_cmd * 2.0f);
+      this->setpoint_.pitch1_out_ = this->pitch1_actr_.Calculate(
+          this->setpoint_.pitch1_theta_, this->pitch1_motor_.raw_speed,
+          this->pitch1_motor_.raw_pos_, this->dt_);
+      this->pitch1_motor_.SetMit(this->setpoint_.pitch1_out_);
       break;
     }
     case RobotArm::WORK_MID: {
-      this->setpoint_.roll1_theta_ += yaw_cmd;
-      this->setpoint_.pitch2_theta_ += pit_cmd;
+      this->setpoint_.pitch2_theta_ = Component::Type::CycleValue(
+          this->setpoint_.pitch2_theta_ + pit_cmd * 2.0f);
+      this->setpoint_.pitch2_out_ = this->pitch2_actr_.Calculate(
+          this->setpoint_.pitch2_theta_, this->pitch2_motor_.raw_speed,
+          this->pitch2_motor_.raw_pos_, this->dt_);
+      this->pitch2_motor_.SetMit(this->setpoint_.pitch2_out_);
 
-      clampf(&(this->setpoint_.pitch2_theta_), this->param_.limit.pitch2_min,
-             this->param_.limit.pitch2_max);
-
-      this->pitch2_motor_.SetPosSpeed(this->setpoint_.pitch2_theta_, 0.3f);
-      this->roll1_motor_.SetPosSpeed(this->setpoint_.roll1_theta_, 0.3f);
       break;
     }
-    case RobotArm::WORK_TOP: {
-      this->setpoint_.yaw2_theta_ += pit_cmd;
-      this->setpoint_roll2_ =
-          Component::Type::CycleValue(this->setpoint_roll2_ + yaw_cmd);
-
-      clampf(&(this->setpoint_.yaw2_theta_), this->param_.limit.yaw2_min,
-             this->param_.limit.yaw2_max);
-
-      this->yaw2_motor_.SetPosSpeed(this->setpoint_.yaw2_theta_, 0.3f);
-
-      float roll2_out = this->roll2_actr_.Calculate(
+    case RobotArm::WORK_BOT: {
+      // yaw2改变、
+      this->setpoint_.yaw2_theta_ = Component::Type::CycleValue(
+          this->setpoint_.yaw2_theta_ + yaw_cmd * 5.0f);
+      this->setpoint_.yaw2_out_ = this->yaw2_actr_.Calculate(
+          this->setpoint_.yaw2_theta_, this->yaw2_motor_.raw_speed,
+          this->yaw2_motor_.raw_pos_, this->dt_);
+      this->yaw2_motor_.SetMit(this->setpoint_.yaw2_out_);
+      //
+      this->setpoint_.roll1_theta_ += pit_cmd * 3.0f;
+      clampf(&(this->setpoint_.roll1_theta_), this->param_.limit.roll1_min,
+             this->param_.limit.roll1_max);
+      this->roll1_motor_.SetPos(this->setpoint_.roll1_theta_);
+      // 维持带代码
+      this->roll2_speed_ = this->roll2_actr_.Calculate(
           this->setpoint_roll2_, this->roll2_motor_.GetSpeed(),
           this->roll2_motor_.GetAngle(), this->dt_);
-      this->roll2_motor_.Control(roll2_out);
-
+      this->roll2_motor_.Control(roll2_speed_);
+      //
       break;
     }
     case RobotArm::SAFE: {
       break;
     }
 
-    case RobotArm::RELAX:
-      this->yaw1_able_ = 0;
-      this->pitch1_able_ = 0;
-      this->pitch2_able_ = 0;
-      this->roll1_able_ = 0;
-      this->yaw2_able_ = 0;
-      this->roll2_motor_.Relax();
+    case RobotArm::RELAX: {
+      if (this->state_.initflag) {  // 上电先读取角度
+        this->DMable();
+        this->state_.initflag = 0;
+
+        for (int i = 0; i < 500; i++) {
+          this->setpoint_.yaw1_theta_ = this->yaw1_motor_.raw_pos_;
+          this->setpoint_.pitch1_theta_ = this->pitch1_motor_.raw_pos_;
+          this->setpoint_.pitch2_theta_ = this->pitch2_motor_.raw_pos_;
+          this->setpoint_.yaw2_theta_ = this->yaw2_motor_.raw_pos_;
+          this->setpoint_.roll1_theta_ = this->roll1_motor_.raw_pos_;
+        }
+      }
       break;
-    case RobotArm::WORK_CUSTOM_CTRL:
-      if (custom_ctrl_.online_) {
-        this->setpoint_.yaw1_theta_ = custom_ctrl_.data_.angle[0];
-        this->setpoint_.pitch1_theta_ = custom_ctrl_.data_.angle[1];
+    }
+    case RobotArm::WORK_CUSTOM_CTRL: {
+      int i = 0;
+      for (i = 0; i < 6; i++) {
+        // this->buffer_.current[i] = custom_ctrl_.data_.angle[i];
+        if (custom_ctrl_.data_.angle[i] < 0.00000000f) {
+          this->buffer_.current[i] = 0.000000f;
 
-        clampf(&(this->setpoint_.yaw1_theta_), this->param_.limit.yaw1_min,
-               this->param_.limit.yaw1_max);
-        clampf(&(this->setpoint_.pitch1_theta_), this->param_.limit.pitch1_min,
-               this->param_.limit.pitch1_max);
-
-        this->yaw1_motor_.SetPosSpeed(this->setpoint_.yaw1_theta_, 0.1f);
-        this->pitch1_motor_.SetPosSpeed(this->setpoint_.pitch1_theta_, 0.3f);
-
-        this->setpoint_.roll1_theta_ = custom_ctrl_.data_.angle[2];
-        this->setpoint_.pitch2_theta_ = custom_ctrl_.data_.angle[3];
-
-        clampf(&(this->setpoint_.pitch2_theta_), this->param_.limit.pitch2_min,
-               this->param_.limit.pitch2_max);
-
-        this->pitch2_motor_.SetPosSpeed(this->setpoint_.pitch2_theta_, 0.3f);
-        this->roll1_motor_.SetPosSpeed(this->setpoint_.roll1_theta_, 0.3f);
-
-        this->setpoint_.yaw2_theta_ = custom_ctrl_.data_.angle[4];
-        this->setpoint_roll2_ = custom_ctrl_.data_.angle[5];
-
-        clampf(&(this->setpoint_.yaw2_theta_), this->param_.limit.yaw2_min,
-               this->param_.limit.yaw2_max);
-
-        this->yaw2_motor_.SetPosSpeed(this->setpoint_.yaw2_theta_, 0.3f);
-
-        float roll2_out = this->roll2_actr_.Calculate(
-            this->setpoint_roll2_, this->roll2_motor_.GetSpeed(),
-            this->roll2_motor_.GetAngle(), this->dt_);
-        this->roll2_motor_.Control(roll2_out);
-      } else {
-        this->yaw1_able_ = 0;
-        this->pitch1_able_ = 0;
-        this->pitch2_able_ = 0;
-        this->roll1_able_ = 0;
-        this->yaw2_able_ = 0;
-        this->roll2_motor_.Relax();
+        } else if (custom_ctrl_.data_.angle[i] > M_2PI) {
+          this->buffer_.current[i] = 6.2831;  // 别写2pi
+        } else {
+          this->buffer_.current[i] = custom_ctrl_.data_.angle[i];
+        }
       }
 
+      this->setpoint_.yaw1_theta_ =
+          Component::Type::CycleValue(this->buffer_.current[5] - 2.0f);
+      this->setpoint_.yaw1_out_ = this->yaw1_actr_.Calculate(
+          this->setpoint_.yaw1_theta_, this->yaw1_motor_.raw_speed,
+          this->yaw1_motor_.raw_pos_, this->dt_);
+      this->yaw1_motor_.SetMit(this->setpoint_.yaw1_out_);
       break;
-      // default:
-      //   XB_ASSERT(false);
-      //   return;
+    }
+
+    default:
+      XB_ASSERT(false);
+      return;
   }
 }
 
@@ -262,26 +289,25 @@ void RobotArm::SetMode(RobotArm::Mode mode) {
   if (mode == this->mode_) {
     return;
   }
+  if (mode != WORK_CUSTOM_CTRL) {
+    this->state_.is_first = 1;  // 切换到其他模式，重置is_first
+  }
   if (mode == WORK_BOT) {
-    this->yaw1_able_ = 1;
-    this->pitch1_able_ = 1;
-    this->pitch2_able_ = 1;
-    this->roll1_able_ = 1;
-    this->yaw2_able_ = 1;
+    this->state_.motor_current = 1;
   }
   if (mode == WORK_MID) {
-    this->yaw1_able_ = 1;
-    this->pitch1_able_ = 1;
-    this->pitch2_able_ = 1;
-    this->roll1_able_ = 1;
-    this->yaw2_able_ = 1;
+    this->state_.motor_current = 1;
   }
   if (mode == WORK_TOP) {
-    this->yaw1_able_ = 1;
-    this->pitch1_able_ = 1;
-    this->pitch2_able_ = 1;
-    this->roll1_able_ = 1;
-    this->yaw2_able_ = 1;
+    this->state_.motor_current = 1;
   }
+  if (mode == SAFE) {
+    this->state_.motor_current = 1;
+  }
+  if (mode == RELAX) {
+    this->state_.motor_current = 0;
+    this->state_.xipan_state_ = 0;
+  }
+
   this->mode_ = mode;
 }
